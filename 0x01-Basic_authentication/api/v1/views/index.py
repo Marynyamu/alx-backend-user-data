489f6bb575485871c42adafe0552c54cce26263b
Mary Nyamu <125545040+Marynyamu@users.noreply.github.com>
2024-02-17 14:10:40 +0300

    Update index.py

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -1,5 +1,5 @@
-
-dule of Index views
+#!/usr/bin/env python3
+""" Module of Index views
 """
 from flask import jsonify, abort
 from api.v1.views import app_views
@@ -7,39 +7,39 @@ from api.v1.views import app_views
 
 @app_views.route('/status', strict_slashes=False)
 def status() -> str:
-        """ GET /api/v1/status
-            Return:
-                      - the status of the API
-                          """
-                              return jsonify({"status": "OK"})
+    """ GET /api/v1/status
+    Return:
+      - the status of the API
+    """
+    return jsonify({"status": "OK"})
 
 
-                              @app_views.route('/stats/', strict_slashes=False)
-                              def stats() -> str:
-                                  """ GET /api/v1/stats
-                                      Return:
-                                                - the number of each objects
-                                                    """
-                                                        from models.user import User
-                                                            stats = {}
-                                                                stats['users'] = User.count()
-                                                                    return jsonify(stats)
+@app_views.route('/stats/', strict_slashes=False)
+def stats() -> str:
+    """ GET /api/v1/stats
+    Return:
+      - the number of each objects
+    """
+    from models.user import User
+    stats = {}
+    stats['users'] = User.count()
+    return jsonify(stats)
 
 
-                                                                    @app_views.route('/unauthorized', strict_slashes=False)
-                                                                    def unauthorized() -> str:
-                                                                        """ GET /api/v1/unauthorized
-                                                                            Return:
-                                                                                      - Aborts
-                                                                                          """
-                                                                                              abort(401)
+@app_views.route('/unauthorized', strict_slashes=False)
+def unauthorized() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+      - Aborts
+    """
+    abort(401)
 
 
-                                                                                              @app_views.route('/forbidden', strict_slashes=False)
-                                                                                              def forbidden() -> str:
-                                                                                                  """
-                                                                                                      GET /api/v1/unauthorized
-                                                                                                          Return:
-                                                                                                                    - Aborts
-                                                                                                                        """
-                                                                                                                            abort(403)i
+@app_views.route('/forbidden', strict_slashes=False)
+def forbidden() -> str:
+    """
+    GET /api/v1/unauthorized
+    Return:
+      - Aborts
+    """
+    abort(403)
